3d5746ebd0b0b71619215f60366d520b75e0dfde
DimasEPS <dimaskld4@gmail.com>
2025-11-16 20:40:24 +0700

    search bar

--- src/pages/Admin/Audit/DokumenAudit.jsx
+++ src/pages/Admin/Audit/DokumenAudit.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { SearchIcon } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
 
 export default function DokumenAudit() {
   return (
-    <div>
-      tes
+    <div className="flex flex-wrap items-center gap-4">
+      <InputGroup className="h-14 max-w-[1080px]">
+        <InputGroupInput
+          placeholder="Cari dokumen berdasarkan nama"
+          className="bg-state text-navy placeholder:text-gray-dark"
+        />
+        <InputGroupAddon>
+          <SearchIcon className="text-navy" />
+        </InputGroupAddon>
+      </InputGroup>
     </div>
-  )
+  );
 }
